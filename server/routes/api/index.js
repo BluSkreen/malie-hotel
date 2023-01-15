c97d668fbad01bae2dc1fa736221f1339067d036
Jason Duran <112275194+BluSkreen@users.noreply.github.com>
2023-01-15 13:07:18 -0800

    Delete server/routes directory

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const matchupRoutes = require('./matchup-routes');
-const techRoutes = require('./tech-routes.js');
-
-router.use('/matchup', matchupRoutes);
-router.use('/tech', techRoutes);
-
-module.exports = router;
