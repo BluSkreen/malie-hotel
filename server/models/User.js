70da548ec5da078389ff211a9acab9a3b3b4cda8
Hiro Protagonist <shdesai327@gmail.com>
2023-01-17 13:21:15 -0700

    added bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,64 @@
 const { Schema, model } = require("mongoose");
-// const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-        },
-        Credit_card_number: {
-            type: Number,
-            required: true,
-        },
-        Credit_card_expiration: {
-            type: Number,
-            required: true,
-        },
-        Credit_card_cvv: {
-            type: Number,
-            required: true,
-        },
-
-        password: {
-            type: String,
-            required: true,
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    Credit_card_number: {
+      type: Number,
+      required: true,
+    },
+    Credit_card_expiration: {
+      type: Number,
+      required: true,
+    },
+    Credit_card_cvv: {
+      type: Number,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-// userSchema.pre("save", async function (next) {
-//   // creating a account/ password is changed
-//   if (this.isNew || this.isModified("password")) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
-// });
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 const User = model("User", userSchema);
 
 module.exports = User;
