8eb9358a2398a81c2064c5ec3c899dffcbccf4de
Erich <eebbinghaus@gmail.com>
2023-01-19 16:46:38 -0700

    created admin page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,6 +5,7 @@ import Modal from "./Modal";
 import GolfCourse from "./GolfCourse";
 import Rooms from "./Rooms"
 import Payment from "./Payment"
+import Admin from "./Admin"
 
-export { Navbar, Home, Footer, Modal, GolfCourse, Rooms, Payment };
+export { Navbar, Home, Footer, Modal, GolfCourse, Rooms, Payment, Admin };
 
