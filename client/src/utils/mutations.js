92a365f0864b5953c38b1087f3232951dd4c8c4d
Ryan <rylarson17@gmail.com>
2023-01-20 13:58:58 -0700

    Modal toggles for account creation and creates a user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-  mutation Mutation($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
+        Credit_card_cvv
+        Credit_card_expiration
+        Credit_card_number
         _id
+        address
+        email
         username
+        isAdmin
+        password
       }
     }
   }
