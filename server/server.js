4e6b58a3c6088783d7e9d0fd2be21108e7b0f3ac
BluSkreen <jason.duran@gmail.com>
2023-01-23 06:08:39 -0700

    testing webhook response

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const server = new ApolloServer({
 const stripe = require('stripe');
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = "whsec_234db6f1bf5247050818477e5b438ed17b3aab52ca2dcba4cb92d65539dfd314";
+// const endpointSecret = "whsec_234db6f1bf5247050818477e5b438ed17b3aab52ca2dcba4cb92d65539dfd314";
+const endpointSecret = "whsec_YZE2oaS9d8ikBJJYQsk9DRhg6AvCGaWe";
 
 //const endpointSecret = process.env.STRIPESECRET;
 
@@ -34,7 +35,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.post('/webhook', (request, response) => {
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
     const sig = request.headers['stripe-signature'];
     //console.log("sig");
     //console.log(sig);
@@ -47,24 +48,6 @@ app.post('/webhook', (request, response) => {
         console.log(request.body.data.object.metadata);
         console.log("payment success");
         makeReservation(request.body.data.object.metadata);
-          //const reservationObj = {
-          //    "roomNumbers": [roomNumber],
-          //    "startDate": startDate,
-          //    "endDate": endDate,
-          //    "cost": cost,
-          //    "email": context.user.email,
-          //    "prodId": paymentId,
-          //    "payment": "pending",
-          //}
-          //const { _id } = await Reservation.create({ ...reservationObj });
-          //const room = await Room.findOneAndUpdate(
-          //    { roomNumber: roomNumber },
-          //    {
-          //        $addToSet: {
-          //            reservations: _id,
-          //        },
-          //    },
-          //);
 
     } else if (request.body.type == 'payment_intent.canceled' || 'payment_intent.canceled') {
         console.log("not successful");
@@ -73,31 +56,15 @@ app.post('/webhook', (request, response) => {
     //return response.status(200);
     try {
         event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        console.log("event");
+        console.log(event);
     } catch (err) {
         response.status(400).send(`Webhook Error: ${err.message}`);
         return;
     }
 
-    //console.log("event");
-    //console.log(event);
-
-    // Handle the event
-
-    //switch (event.type) {
-    //    case 'payment_intent.succeeded':
-    //        const paymentIntent = event.data.object;
-    //        console.log("paymentIntent");
-    //        console.log(paymentIntent);
-
-    //        // Then define and call a function to handle the event payment_intent.succeeded
-    //        break;
-            // ... handle other event types
-    //    default:
-    //        console.log(`Unhandled event type ${event.type}`);
-    //}
-
     // Return a 200 response to acknowledge receipt of the event
-    return response.status(200);
+    response.json({received: true});
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -119,18 +86,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
 
-// server.js
-//
-// Use this sample code to handle webhook events in your integration.
-//
-// 1) Paste this code into a new file (server.js)
-//
-// 2) Install dependencies
-//   npm install stripe
-//   npm install express
-//
-// 3) Run the server on http://localhost:4242
-//   node server.js
-
-
-// webhook_app.listen(4242, () => console.log('Running on port 4242'));
